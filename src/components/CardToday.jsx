aeb30f48ce2cb7b9f5be647e8b0c85cbb5acac7c
loyolamayahugo <hugo.loyola@wizeline.com>
2022-12-15 10:54:51 -0600

    destrucuring of data

--- src/components/CardToday.jsx
+++ src/components/CardToday.jsx
@@ -1,5 +1,7 @@
 
 export const CardToday = ({ data, date, imgUrl, error, setDatePiker }) => {
+  const {url, title, explanation } = data
+  
   return (
     <div className="row">
       <div className="col-md-6 col-sm-12 container_img">
@@ -8,18 +10,18 @@ export const CardToday = ({ data, date, imgUrl, error, setDatePiker }) => {
             <iframe
               title="myFrame"
               className="embed-responsive-item"
-              src={data.url}
+              src={url}
             ></iframe>
           </div>
         ) : (
-          <img className="img_nasa" src={data.url} alt="picture_day" />
+          <img className="img_nasa" src={url} alt="picture_day" />
         )}
       </div>
 
       <div className="col-md-6 col-sm-12">
-        <h2 className="title">{data.title}</h2>
+        <h2 className="title">{title}</h2>
         <p>{error}</p>
-        <p className="body_text">{data.explanation}</p>
+        <p className="body_text">{explanation}</p>
 
         <h3 className="body_text">Selec date: {date}</h3>
         <div>
